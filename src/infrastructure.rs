9af8b2290d14309946a84fa77aec25b412db28ee
George Tsiamasiotis <gtsiam@windowslive.com>
2021-05-25 23:27:07 +0300

    Add /ping support

--- src/infrastructure.rs
+++ src/infrastructure.rs
@@ -1,1 +1,25 @@
+use crate::{errors::Errors, Client, Result};
 
+impl Client {
+    pub async fn ping(&self) -> Result<()> {
+        let endpoint = self.base_url.join("/ping")?;
+
+        let res = self.http.get(endpoint).send().await?;
+        if res.text().await? == "pong" {
+            Ok(())
+        } else {
+            Err(Errors::PingError)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn ping_server() {
+        let client = Client::new().unwrap();
+        client.ping().await.unwrap();
+    }
+}
